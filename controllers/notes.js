82f54db67b04036c5258f032ecd8e5a964011b75
AmeenKibria <makibria@gmail.com>
2020-11-01 18:01:20 +0200

    done up to 4.6

---
+++ controllers/notes.js
@@ -0,0 +1,74 @@
+const notesRouter = require('express').Router()
+const Note = require('../models/note')
+
+
+//Fetching all notes
+notesRouter.get('/', async (request, response) => {
+    const notes = await Note.find ({})
+    response.json(notes)
+})
+
+//Fetching particular note by id
+notesRouter.get('/:id', async (request, response, next) => {
+  try {
+    const note = await Note.findById(request.params.id)
+    if (note) {
+      response.json (note)
+    }else {
+      response.status(404).end()
+    }
+  }catch (exception) {
+    next(exception)
+  }
+})
+
+
+//Adding new note 
+notesRouter.post('/', async (request, response, next) => {
+  const body = request.body
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date()
+  })
+  try{
+    const savedNote = await note.save()
+    response.json(savedNote)
+  } catch (exception){
+    next (exception)
+  }
+})
+
+
+//Deleting exiting note
+notesRouter.delete('/:id', async (request, response, next) => {
+  try{
+    await Note.findByIdAndRemove(request.params.id)
+
+      response.status(204).end()
+    }catch(exception) {
+      next (exception)
+    }
+})
+
+
+//Updating notes
+notesRouter.put('/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+
+
+module.exports = notesRouter
